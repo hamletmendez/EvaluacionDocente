59c918e7a3ce994f2b6deaa86c34e601fcfc750f
Hamlet Méndez <hamletmendez@hotmail.com>
2017-11-10 09:09:02 -0400

    Shared Preferences

--- app/src/main/java/com/example/evaluaciondocente/PreferencesManager.java
+++ app/src/main/java/com/example/evaluaciondocente/PreferencesManager.java
@@ -27,10 +27,13 @@ public class PreferencesManager {
         editor = preferences.edit();
     }
 
+    //Cambiar el estado de la preferencia
     public void setFirstTimeLaunch(boolean isFirstTime){
         editor.putBoolean(IS_FIRST_TIME_LAUNCH,isFirstTime);
         editor.commit();
     }
+
+    //Verificar si es la primera vez que entra a la opción
     public boolean isFirstTimeLaunch(){
         return preferences.getBoolean(IS_FIRST_TIME_LAUNCH,true);
     }
